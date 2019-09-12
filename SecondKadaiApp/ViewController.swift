5a47a302e3dd4389fda77fec986b7e8741039ef9
白鳥雅人 <whitesplash0703@icloud.com>
2019-09-12 20:43:32 +0900

    名前を入力すると、画面遷移し、名前を表示されるようにした。 AoutoLayoutを使い、画面中央に表示した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -13,15 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        func prepare(for segue:UIStoryboardSegue, sender: Any?){
-            //segueから遷移先のResultViewControllerを取得
-            let _:ResultViewController = segue.destination as! ResultViewController
-            //遷移先のResultViewControllerで宣言しているtextに値を代入して渡す
-            resultViewController.text = UITextField.text
-        }
+        
         
         
     }
+    override func prepare(for segue:UIStoryboardSegue, sender: Any?){
+        //segueから遷移先のResultViewControllerを取得
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        //遷移先のResultViewControllerで宣言しているtextに値を代入して渡す
+        resultViewController.moji = MyTextField.text!
+        
+        
+    }
+    // TextFieldをOutlet接続する
+    @IBOutlet weak var MyTextField: UITextField!
+    // 遷移先から戻ってくるときのメソッド
     @IBAction func unwind(_ segue: UIStoryboardSegue){
         
     }
